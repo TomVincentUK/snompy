464e437d6b4953dd72ccfce542098c07d0008dbb
Tom Vincent <TomVincentCode@gmail.com>
2022-06-17 21:09:44 +0100

    Changing module to a package (perhaps prematurely)

---
+++ finite_dipole/__init__.py
@@ -0,0 +1,17 @@
+from .finite_dipole import (
+    complex_quad,
+    refl_coeff,
+    geometry_function,
+    eff_polarizability,
+    Fourier_envelope,
+    eff_polarizability_nth,
+)
+
+__all__ = [
+    complex_quad,
+    refl_coeff,
+    geometry_function,
+    eff_polarizability,
+    Fourier_envelope,
+    eff_polarizability_nth,
+]
