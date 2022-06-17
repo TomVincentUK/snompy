cd3d814a058d496f99367693c3700a0bbfd7fef1
Tom Vincent <TomVincentCode@gmail.com>
2022-06-17 21:22:10 +0100

    Added setup.py

---
+++ setup.py
@@ -0,0 +1,22 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+with open("requirements.txt", "r") as fh:
+    requirements = [line.strip() for line in fh]
+
+setuptools.setup(
+    name="finite-dipole",
+    version="0.0.1",
+    author="Tom Vincent",
+    author_email="tom.vincent@manchester.ac.uk",
+    description="A Python implementation of the finite dipole model for scanning near-field optical microscopy contrast.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+    ],  # TO ADD: LICENSE, OS
+    python_requires=">=3.10",
+    install_requires=requirements,
+)
