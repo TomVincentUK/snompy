1f7b4676b10d77ac2062334900c253ee1529d171
Tom Vincent <tomvincentcode@gmail.com>
2022-07-21 09:20:24 +0100

    Multilayer code working but very slowly

--- finite_dipole/multilayer.py
+++ finite_dipole/multilayer.py
@@ -18,49 +18,10 @@ References
 import numpy as np
 from numba import njit
 
-from .tools import complex_quad
+from .tools import refl_coeff, complex_quad, Fourier_envelope
 
-
-@njit
-def geom_func_ML(z, x, radius, semi_maj_axis, g_factor):
-    """
-    Function that encapsulates the geometric properties of the tip-sample
-    system. Defined as :math:`f_0` or :math:`f_1` in equation (11) of
-    reference [1]_, for multilayer samples.
-
-    Parameters
-    ----------
-    z : float
-        Height of the tip above the sample. Defined as :math:`H` in
-        reference [1]_.
-    x : float
-        Position of an induced charge within the tip. Specified relative to
-        the tip radius. Defined as :math:`X_0` or :math:`X_1` in equation
-        (11) of reference [1]_.
-    radius : float
-        Radius of curvature of the AFM tip in metres. Defined as
-        :math:`\rho` in reference [1]_.
-    semi_maj_axis : float
-        Semi-major axis in metres of the effective spheroid from the FDM.
-        Defined as :math:`L` in reference [1]_.
-    g_factor : complex
-        A dimensionless approximation relating the magnitude of charge
-        induced in the AFM tip to the magnitude of the nearby charge which
-        induced it. A small imaginary component can be used to account for
-        phase shifts caused by the capacitive interaction of the tip and
-        sample. Defined as :math:`g` in reference [1]_.
-
-    Returns
-    -------
-    f_n : complex
-        A complex number encapsulating geometric properties of the tip-
-        sample system.
-    """
-    return (
-        (g_factor - (radius + z + x * radius) / (2 * semi_maj_axis))
-        * np.log(4 * semi_maj_axis / (radius + 2 * z + 2 * x * radius))
-        / np.log(4 * semi_maj_axis / radius)
-    )
+EPS_STACK_ERR = ValueError("`eps_stack` must have length 2 greater than `t_stack`.")
+BETA_STACK_ERR = ValueError("`beta_stack` must have length 1 greater than `t_stack`.")
 
 
 def refl_coeff_ML(beta_stack, t_stack):
@@ -81,18 +42,18 @@ def refl_coeff_ML(beta_stack, t_stack):
 
     Returns
     -------
-    beta_q : function
+    beta_k : function
         A scalar function of momentum, `q`, which returns the complex
         effective reflection coefficient for the stack.
     """
     if len(beta_stack) != len(t_stack) + 1:
-        raise ValueError("`beta_stack` must have length 1 greater than `t_stack`.")
+        raise BETA_STACK_ERR
 
     if len(beta_stack) == 1:
         beta_final = beta_stack[0]
 
         @njit
-        def beta_q(q):
+        def beta_k(k):
             return beta_final
 
     else:
@@ -104,39 +65,227 @@ def refl_coeff_ML(beta_stack, t_stack):
         beta_next = refl_coeff_ML(beta_stack_next, t_stack_next)
 
         @njit
-        def beta_q(q):
-            next_layer = beta_next(q) * np.exp(-2 * q * t_current)
+        def beta_k(k):
+            next_layer = beta_next(k) * np.exp(-2 * k * t_current)
             return (beta_current + next_layer) / (1 + beta_current * next_layer)
 
-    return beta_q
+    return beta_k
+
+
+@njit
+def _k_phi_weight(k, z_q):
+    """Used by potential_0()"""
+    return np.exp(-k * 2 * z_q)
+
+
+def potential_0(z_q, beta_k):
+    def _integrand(xi):
+        k = xi / z_q  # xi is just k adjusted to the characteristic length scale
+        return beta_k(k) * _k_phi_weight(k, z_q)
+
+    integral, error = complex_quad(_integrand, 0, np.inf)
+
+    # Rescale from xi to k
+    integral /= z_q
+    error /= z_q
+    return integral, error
+
+
+@njit
+def _k_E_weight(k, z_q):
+    """Used by E_z_0()"""
+    return -k * np.exp(-k * 2 * z_q)
+
+
+def E_z_0(z_q, beta_k):
+    def _integrand(xi):
+        k = xi / z_q  # xi is just k adjusted to the characteristic length scale
+        return beta_k(k) * _k_E_weight(k, z_q)
+
+    integral, error = complex_quad(_integrand, 0, np.inf)
+
+    # Rescale from xi to k
+    integral /= z_q
+    error /= z_q
+    return integral, error
+
+
+def eff_charge_and_pos(z_q, beta_k):
+    phi, _ = potential_0(z_q, beta_k)
+    E, _ = E_z_0(z_q, beta_k)
+
+    # Add in error propagation here too?
+
+    z_image = np.abs(phi / E) - z_q
+    beta_image = -(phi**2) / E
+    return z_image, beta_image
+
+
+@njit
+def geom_func_ML(z, z_q, radius, semi_maj_axis, g_factor):
+    """
+    Function that encapsulates the geometric properties of the tip-sample
+    system. Defined as :math:`f_0` or :math:`f_1` in equation (11) of
+    reference [1]_, for multilayer samples.
+
+    Parameters
+    ----------
+    z : float
+        Height of the tip above the sample. Defined as :math:`H` in
+        reference [1]_.
+    z_q : float
+        Position of an image charge below the surface. Defined as
+        :math:`X_0` or :math:`X_1` in equation (11) of reference [1]_.
+    radius : float
+        Radius of curvature of the AFM tip in metres. Defined as
+        :math:`\rho` in reference [1]_.
+    semi_maj_axis : float
+        Semi-major axis in metres of the effective spheroid from the FDM.
+        Defined as :math:`L` in reference [1]_.
+    g_factor : complex
+        A dimensionless approximation relating the magnitude of charge
+        induced in the AFM tip to the magnitude of the nearby charge which
+        induced it. A small imaginary component can be used to account for
+        phase shifts caused by the capacitive interaction of the tip and
+        sample. Defined as :math:`g` in reference [1]_.
+
+    Returns
+    -------
+    f_n : complex
+        A complex number encapsulating geometric properties of the tip-
+        sample system.
+    """
+    return (
+        (g_factor - (radius + z + z_q) / (2 * semi_maj_axis))
+        * np.log(4 * semi_maj_axis / (radius + 2 * z + 2 * z_q))
+        / np.log(4 * semi_maj_axis / radius)
+    )
+
+
+def eff_pol_0_ML(z, beta_k, W_0, W_1, radius, semi_maj_axis, g_factor):
+    z_q_0 = z + radius * W_0
+    z_q_1 = z + radius * W_1
+    z_im_0, beta_im_0 = eff_charge_and_pos(z_q_0, beta_k)
+    z_im_1, beta_im_1 = eff_charge_and_pos(z_q_1, beta_k)
+    f_0 = geom_func_ML(z, z_im_0, radius, semi_maj_axis, g_factor)
+    f_1 = geom_func_ML(z, z_im_1, radius, semi_maj_axis, g_factor)
+    return 1 + (beta_im_0 * f_0) / (2 * (1 - beta_im_1 * f_1))
+
+
+def _integrand_ML(
+    t,
+    z,
+    tapping_amplitude,
+    harmonic,
+    beta_k,
+    W_0,
+    W_1,
+    radius,
+    semi_maj_axis,
+    g_factor,
+):
+    """
+    Function to be integrated from -pi to pi, to extract the Fourier
+    component of `eff_pol_0_ML()` corresponding to `harmonic`.
+    """
+    alpha_eff = eff_pol_0_ML(
+        z + tapping_amplitude * (1 + np.cos(t)),
+        beta_k,
+        W_0,
+        W_1,
+        radius,
+        semi_maj_axis,
+        g_factor,
+    )
+    sinusoids = Fourier_envelope(t, harmonic)
+    return alpha_eff * sinusoids
 
 
 @np.vectorize
-def potential(z, z_q, beta_q):
-    @njit
-    def _integrand(xi):
-        return beta_q(xi / z_q) * np.exp(-xi * (2 * z_q + z) / z_q)
+def _integral_ML(
+    z,
+    tapping_amplitude,
+    harmonic,
+    beta_k,
+    W_0,
+    W_1,
+    radius,
+    semi_maj_axis,
+    g_factor,
+):
+    """
+    Function that extracts the Fourier component of `eff_pol_0_ML()`
+    corresponding to `harmonic`.
+    """
+    return complex_quad(
+        _integrand_ML,
+        -np.pi,
+        np.pi,
+        args=(
+            z,
+            tapping_amplitude,
+            harmonic,
+            beta_k,
+            W_0,
+            W_1,
+            radius,
+            semi_maj_axis,
+            g_factor,
+        ),
+    )
 
-    integral, error = complex_quad(_integrand, 0, np.inf)
-    return integral / z_q, error / z_q
 
+def eff_pol_ML(
+    z,
+    tapping_amplitude,
+    harmonic,
+    eps_stack=None,
+    beta_stack=None,
+    t_stack=[],
+    W_0=None,
+    W_1=0.5,
+    radius=20e-9,
+    semi_maj_axis=300e-9,
+    g_factor=0.7 * np.exp(0.06j),
+    return_err=False,
+):
+    # beta calculated from eps_sample if not specified
+    if eps_stack is None:
+        if beta_stack is None:
+            raise ValueError("Either `eps_stack` or `beta_stack` must be specified.")
+    else:
+        if beta_stack is None:
+            if len(eps_stack) != len(t_stack) + 2:
+                raise EPS_STACK_ERR
+            beta_stack = refl_coeff(eps_stack[:-1], eps_stack[1:])
+        else:
+            warnings.warn("`beta_stack` overrides `eps_stack` when both are specified.")
 
-def eff_charge_and_pos(z_q, beta_q, d_z=1e-9):
-    phi_0, _ = potential(0, z_q, beta_q)
-    phi_p, _ = potential(d_z, z_q, beta_q)
-    phi_m, _ = potential(-d_z, z_q, beta_q)
-    phi_gradient = (phi_p + phi_m - 2 * phi_0) / d_z**2
+    if len(beta_stack) != len(t_stack) + 1:
+        raise BETA_STACK_ERR
 
-    # Need to add in error propagation here too
+    if W_0 is None:
+        W_0 = 1.31 * semi_maj_axis / (semi_maj_axis + 2 * radius)
 
-    X = np.abs(phi_0 / phi_gradient) - z_q
-    beta_X = -(phi_0**2) / phi_gradient
-    return X, beta_X
+    beta_k = refl_coeff_ML(beta_stack, t_stack)
 
-@njit
-def eff_pol_0_ML(z, beta_q, x_0, x_1, radius, semi_maj_axis, g_factor):
-    X_0, beta_0 = eff_charge_and_pos(x_0, beta_q)
-    X_1, beta_1 = eff_charge_and_pos(x_1, beta_q)
-    f_0 = geom_func_ML(z, X_0, radius, semi_maj_axis, g_factor)
-    f_1 = geom_func_ML(z, X_1, radius, semi_maj_axis, g_factor)
-    return 1 + (beta_0 * f_0) / (2 * (1 - beta_1 * f_1))
+    alpha_eff, alpha_eff_err = _integral_ML(
+        z,
+        tapping_amplitude,
+        harmonic,
+        beta_k,
+        W_0,
+        W_1,
+        radius,
+        semi_maj_axis,
+        g_factor,
+    )
+
+    # Normalize to period of integral
+    alpha_eff /= 2 * np.pi
+    alpha_eff_err /= 2 * np.pi
+
+    if return_err:
+        return alpha_eff, alpha_eff_err
+    else:
+        return alpha_eff
--- refl_coeff_ML_experiments.py
+++ refl_coeff_ML_experiments.py
@@ -8,14 +8,26 @@ eps_air = 1
 eps_PS = 2.5
 eps_PMMA = 1.52 + 0.83j
 eps_Si = 11.7
-eps_stack = eps_air, eps_PS, eps_Si
+eps_stack = eps_air, eps_PMMA, eps_Si
 
 t_PS = 100e-9
 t_PMMA = 100e-9
-t_stack = [t_PS,]
+t_stack = (t_PMMA,)
 
-beta_stack = fdm.tools.refl_coeff(eps_stack[1:], eps_stack[:1])
-beta_q = fdm.multilayer.refl_coeff_ML(beta_stack, t_stack)
+beta_stack = fdm.tools.refl_coeff(eps_stack[:1], eps_stack[1:])
+beta_k = fdm.multilayer.refl_coeff_ML(beta_stack, t_stack)
 
-z_q = 10e-9
-X, beta_X = fdm.multilayer.eff_charge_and_pos(z_q, beta_q)
+z_0 = np.linspace(0, 35, 12)[..., np.newaxis] * 1e-9
+tapping_amplitude = 20e-9
+harmonic = np.arange(2, 4, 1)
+
+import time
+start = time.perf_counter()
+alpha = fdm.multilayer.eff_pol_ML(
+    z_0, tapping_amplitude, harmonic, beta_stack=beta_stack, t_stack=t_stack
+)
+end = time.perf_counter()
+print(start - end)
+
+plt.plot(z_0, np.abs(alpha))
+plt.show()
