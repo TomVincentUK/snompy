9ba69d18e81ab1e33fe3469ef23dc71c9f105229
Tom Vincent <TomVincentCode@gmail.com>
2022-11-29 23:29:26 +0000

    Added further tests for demodulation

--- finite_dipole/demodulate.py
+++ finite_dipole/demodulate.py
@@ -4,15 +4,18 @@ Demodulation code.
 import numpy as np
 from scipy.integrate import trapezoid, simpson
 from numba import njit
+from numba.extending import is_jitted
 
 
-@njit
 def _sampled_integrand(f_x, x_0, x_amplitude, harmonic, f_args, n_samples):
     theta = np.linspace(-np.pi, np.pi, n_samples)
     x = x_0 + x_amplitude * np.cos(theta)
     f = f_x(x, *f_args)
     envelope = np.exp(-1j * harmonic * theta)
     return f * envelope
+
+
+_sampled_integrand_compiled = njit(_sampled_integrand)
 
 
 def demod(
@@ -27,11 +30,6 @@ def demod(
     if method not in ["trapezium", "simpson", "adaptive"]:
         raise ValueError("`method` must be 'trapezium', 'simpson' or 'adaptive'.")
 
-    x_0, x_amplitude, harmonic, *f_args = np.broadcast_arrays(
-        *(x_0, x_amplitude, harmonic) + f_args
-    )
-    f_args = tuple(f_args)
-
     if method == "adaptive":
         raise NotImplementedError()
     else:
@@ -39,11 +37,15 @@ def demod(
             np.array(arr)[..., np.newaxis]
             for arr in np.broadcast_arrays(*(x_0, x_amplitude, harmonic) + f_args)
         ]
-        f_args = tuple(f_args)
+        print(x_0.shape)
 
-        integrand = _sampled_integrand(
-            f_x, x_0, x_amplitude, harmonic, f_args, n_samples
-        )
+        # Function falls back to uncompiled code if not given jitted f_x
+        if is_jitted(f_x):
+            si = _sampled_integrand_compiled
+            f_args = tuple(f_args)
+        else:
+            si = _sampled_integrand
+        integrand = si(f_x, x_0, x_amplitude, harmonic, f_args, n_samples)
 
         if method == "trapezium":
             result = trapezoid(integrand) / (n_samples - 1)
--- finite_dipole/tests/test_demod.py
+++ finite_dipole/tests/test_demod.py
@@ -8,7 +8,6 @@ methods = "trapezium", "simpson"
 
 @pytest.mark.parametrize("method", methods)
 def test_only_zeroth_harmonic_with_constant_function(method):
-    @njit
     def constant_function(x):
         return np.ones_like(x)
 
@@ -24,11 +23,51 @@ def test_only_zeroth_harmonic_with_constant_function(method):
 
 @pytest.mark.parametrize("method", methods)
 def test_only_first_harmonic_with_linear_function(method):
-    @njit
     def linear_function(x):
-        return x
+        return 2 * x
 
     result = demod(
         f_x=linear_function, x_0=0, x_amplitude=1, harmonic=np.arange(3), method=method
     )
-    np.testing.assert_almost_equal(result, [0, 0.5, 0])
+    np.testing.assert_almost_equal(result, [0, 1, 0])
+
+
+@pytest.mark.parametrize("method", methods)
+def test_jitted_and_nonjitted_f_x(method):
+    def nonjitted_function(x):
+        return x
+
+    jitted_function = njit(nonjitted_function)
+
+    nonjitted_result = demod(
+        f_x=nonjitted_function, x_0=0, x_amplitude=1, harmonic=1, method=method
+    )
+    jitted_result = demod(
+        f_x=jitted_function, x_0=0, x_amplitude=1, harmonic=1, method=method
+    )
+    assert nonjitted_result == jitted_result
+
+
+@pytest.mark.parametrize("method", methods)
+def test_broadcasting(method):
+    def function_with_args(x, a, b, c):
+        return x + a + b + c
+
+    x_0 = 0
+    x_amplitude = np.arange(2)
+    harmonic = np.arange(3)[:, np.newaxis]
+    a = np.arange(4)[:, np.newaxis, np.newaxis]
+    b = np.arange(5)[:, np.newaxis, np.newaxis, np.newaxis]
+    c = np.arange(6)[:, np.newaxis, np.newaxis, np.newaxis, np.newaxis]
+    target_shape = (x_0 + x_amplitude + harmonic + a + b + c).shape
+
+    result = demod(
+        f_x=function_with_args,
+        x_0=x_0,
+        x_amplitude=x_amplitude,
+        harmonic=harmonic,
+        method=method,
+        f_args=(a, b, c),
+    )
+
+    assert result.shape == target_shape
