a45c38f37a815a6a2224b664a7014f144836f091
Tom Vincent <tomvincentcode@gmail.com>
2023-01-03 15:25:41 +0000

    Made broadcasting work with beta_k functions

--- finite_dipole/multilayer.py
+++ finite_dipole/multilayer.py
@@ -29,16 +29,16 @@ def _beta_and_t_stack_from_inputs(eps_stack, beta_stack, t_stack):
     if t_stack is None:
         t_stack = np.array([])
     else:
-        t_stack = np.asarray(t_stack)
+        t_stack = np.asarray(np.broadcast_arrays(*t_stack))
 
     if eps_stack is None:
         if beta_stack is None:
             raise ValueError("Either `eps_stack` or `beta_stack` must be specified.")
-        beta_stack = np.asarray(beta_stack)
+        beta_stack = np.asarray(np.broadcast_arrays(*beta_stack))
     else:
         if beta_stack is None:
             # beta_stack calculated from eps_stack if not specified
-            eps_stack = np.asarray(eps_stack)
+            eps_stack = np.asarray(np.broadcast_arrays(*eps_stack))
             if eps_stack.shape[0] != t_stack.shape[0] + 2:
                 raise ValueError(
                     "`eps_stack` must be 2 longer than `t_stack` along the first axis."
@@ -46,7 +46,7 @@ def _beta_and_t_stack_from_inputs(eps_stack, beta_stack, t_stack):
             beta_stack = refl_coeff(eps_stack[:-1], eps_stack[1:])
         else:
             warnings.warn("`beta_stack` overrides `eps_stack` when both are specified.")
-            beta_stack = np.asarray(beta_stack)
+            beta_stack = np.asarray(np.broadcast_arrays(*beta_stack))
 
     if beta_stack.shape[0] != t_stack.shape[0] + 1:
         raise ValueError(
@@ -106,6 +106,10 @@ def refl_coeff_ML(eps_stack=None, beta_stack=None, t_stack=None):
     """
     beta_stack, t_stack = _beta_and_t_stack_from_inputs(eps_stack, beta_stack, t_stack)
     beta_k = _beta_func_from_stack(beta_stack, t_stack)
+
+    # Ensure np.ndim(beta_k) = np.ndim(beta_k(scalar))
+    beta_k.ndim = np.ndim(beta_k(0))
+
     return beta_k
 
 
--- finite_dipole/tests/test_multilayer.py
+++ finite_dipole/tests/test_multilayer.py
@@ -4,6 +4,7 @@ import pytest
 from finite_dipole.multilayer import (
     _beta_and_t_stack_from_inputs,
     _beta_func_from_stack,
+    eff_pol_ML,
 )
 
 VALID_EPS_AND_T_STACK_PAIRS = [
@@ -58,14 +59,6 @@ def test_beta_and_t_stack_from_inputs_eps_returns_arrays(eps_stack, t_stack):
     assert type(beta_stack) == type(t_stack_new) == np.ndarray
 
 
-@pytest.mark.parametrize("eps_stack, t_stack", VALID_EPS_AND_T_STACK_PAIRS)
-def test_beta_and_t_stack_from_inputs_eps_leaves_t_unchanged(eps_stack, t_stack):
-    beta_stack, t_stack_new = _beta_and_t_stack_from_inputs(
-        eps_stack=eps_stack, beta_stack=None, t_stack=t_stack
-    )
-    np.testing.assert_equal(t_stack_new, t_stack)
-
-
 @pytest.mark.parametrize("beta_stack, t_stack", VALID_BETA_AND_T_STACK_PAIRS)
 def test_beta_and_t_stack_from_inputs_beta_creates_right_shape(beta_stack, t_stack):
     beta_stack_new, t_stack_new = _beta_and_t_stack_from_inputs(
@@ -83,14 +76,6 @@ def test_beta_and_t_stack_from_inputs_beta_returns_arrays(beta_stack, t_stack):
 
 
 @pytest.mark.parametrize("beta_stack, t_stack", VALID_BETA_AND_T_STACK_PAIRS)
-def test_beta_and_t_stack_from_inputs_beta_leaves_t_unchanged(beta_stack, t_stack):
-    beta_stack_new, t_stack_new = _beta_and_t_stack_from_inputs(
-        eps_stack=None, beta_stack=beta_stack, t_stack=t_stack
-    )
-    np.testing.assert_equal(t_stack_new, t_stack)
-
-
-@pytest.mark.parametrize("beta_stack, t_stack", VALID_BETA_AND_T_STACK_PAIRS)
 def test_beta_and_t_stack_from_inputs_beta_leaves_beta_unchanged(beta_stack, t_stack):
     beta_stack_new, t_stack_new = _beta_and_t_stack_from_inputs(
         eps_stack=None, beta_stack=beta_stack, t_stack=t_stack
@@ -102,3 +87,37 @@ def test_beta_and_t_stack_from_inputs_beta_leaves_beta_unchanged(beta_stack, t_s
 def test_beta_func_from_stack_creates_right_shape(beta_stack, t_stack):
     beta_k = _beta_func_from_stack(np.asarray(beta_stack), np.asarray(t_stack))
     assert np.shape(beta_k(1)) == np.shape(beta_stack)[1:]
+
+
+def test_eff_pol_ML_broadcasting():
+    # Measurement parameters
+    wavenumber = np.linspace(1680, 1780, 32) * 1e2
+    z = 50e-9
+    tapping_amplitude = 50e-9
+    harmonic = np.arange(2, 5)[:, np.newaxis]
+    thickness = np.arange(1, 100, 16)[:, np.newaxis, np.newaxis] * 1e-9
+
+    # Eventual output shape should match broadcast arrays
+    target_shape = (wavenumber + z + tapping_amplitude + harmonic + thickness).shape
+
+    # Constant sub- and superstrate dielectric functions
+    eps_super = 1
+    eps_sub = 11.7
+
+    # Dispersive middle layer dielectric function
+    eps_inf = 2
+    osc_freq = 1740e2
+    osc_width = 20e2
+    osc_strength = 15e-3
+    eps_middle = eps_inf + (osc_strength * osc_freq**2) / (
+        osc_freq**2 - wavenumber**2 - 1j * osc_width * wavenumber
+    )
+
+    alpha_eff = eff_pol_ML(
+        z=z,
+        tapping_amplitude=tapping_amplitude,
+        harmonic=harmonic,
+        eps_stack=(eps_super, eps_middle, eps_sub),
+        t_stack=(thickness,),
+    )
+    assert alpha_eff.shape == target_shape
