bdce38d08e61323ea374099bd400b8466fad0861
Tom Vincent <tomvincentcode@gmail.com>
2023-01-04 13:07:36 +0000

    Reduced resolution of multilayer tests

--- finite_dipole/tests/test_multilayer.py
+++ finite_dipole/tests/test_multilayer.py
@@ -92,11 +92,11 @@ def test_beta_func_from_stack_creates_right_shape(beta_stack, t_stack):
 
 def test_eff_pol_ML_broadcasting():
     # Measurement parameters
-    wavenumber = np.linspace(1680, 1780, 32) * 1e2
+    wavenumber = np.linspace(1680, 1780, 16) * 1e2
     z = 50e-9
     tapping_amplitude = 50e-9
     harmonic = np.arange(2, 5)[:, np.newaxis]
-    thickness = np.arange(1, 100, 16)[:, np.newaxis, np.newaxis] * 1e-9
+    thickness = np.arange(1, 100, 8)[:, np.newaxis, np.newaxis] * 1e-9
 
     # Eventual output shape should match broadcast arrays
     target_shape = (wavenumber + z + tapping_amplitude + harmonic + thickness).shape
@@ -126,9 +126,9 @@ def test_eff_pol_ML_broadcasting():
 
 def test_eff_pol_ML_approach_curve_decays():
     alpha_eff = eff_pol_ML(
-        z=np.linspace(0, 100, 128) * 1e-9,
+        z=np.linspace(0, 100, 16) * 1e-9,
         tapping_amplitude=50e-9,
-        harmonic=np.arange(2, 10)[:, np.newaxis],
+        harmonic=np.arange(2, 5)[:, np.newaxis],
         eps_stack=(1, 2 + 1j, 11.7),
         t_stack=(100e-9,),
     )
@@ -139,7 +139,7 @@ def test_eff_pol_ML_harmonics_decay():
     alpha_eff = eff_pol_ML(
         z=50e-9,
         tapping_amplitude=50e-9,
-        harmonic=np.arange(2, 10),
+        harmonic=np.arange(2, 5),
         eps_stack=(1, 2 + 1j, 11.7),
         t_stack=(100e-9,),
     )
@@ -150,14 +150,14 @@ def test_eff_pol_ML_zero_thickness_layer_invisible():
     alpha_eff = eff_pol_ML(
         z=50e-9,
         tapping_amplitude=50e-9,
-        harmonic=np.arange(2, 10),
+        harmonic=np.arange(2, 5),
         eps_stack=(1, 2 + 1j, 11.7),
         t_stack=(100e-9,),
     )
     alpha_eff_with_zero_thickness_layer = eff_pol_ML(
         z=50e-9,
         tapping_amplitude=50e-9,
-        harmonic=np.arange(2, 10),
+        harmonic=np.arange(2, 5),
         eps_stack=(1, 2 + 1j, 3, 11.7),
         t_stack=(100e-9, 0),
     )
@@ -168,13 +168,13 @@ def test_eff_pol_ML_two_layers_same_as_bulk():
     alpha_eff_bulk = eff_pol(
         z=50e-9,
         tapping_amplitude=50e-9,
-        harmonic=np.arange(2, 10),
+        harmonic=np.arange(2, 5),
         eps_sample=11.7,
     )
     alpha_eff_ML = eff_pol_ML(
         z=50e-9,
         tapping_amplitude=50e-9,
-        harmonic=np.arange(2, 10),
+        harmonic=np.arange(2, 5),
         eps_stack=(1, 11.7),
     )
     np.testing.assert_almost_equal(alpha_eff_bulk, alpha_eff_ML)
