119c08e41410a6472312a4cb64e38571d185a34a
Tom Vincent <tomvincentcode@gmail.com>
2023-02-13 13:51:44 +0000

    Created list of colours

---
+++ docs/_static/palette.py
@@ -0,0 +1,12 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from matplotlib.colors import to_hex
+
+filename = r"palette.txt"
+cmap = plt.cm.Spectral
+palette = np.array(
+    [cmap._segmentdata[component][:, 1] for component in ("red", "green", "blue")]
+).T
+
+with open(filename, "w") as file:
+    file.writelines([to_hex(c) + "\n" for c in palette])
