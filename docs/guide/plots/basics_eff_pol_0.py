6fc8ac6b06392eb25d7ee1ee69f4f838b628a812
Tom Vincent <tomvincentcode@gmail.com>
2023-03-03 17:07:21 +0000

    Working on demod docs

--- docs/guide/plots/basics_eff_pol_0.py
+++ docs/guide/plots/basics_eff_pol_0.py
@@ -3,8 +3,10 @@ import numpy as np
 
 import pysnom
 
-# Set some experimental parameters for an AFM approach curve
-z = np.linspace(0, 200e-9, 512)  # Define an approach curve
+# Set the height for an AFM approach curve
+z = np.linspace(0, 200e-9, 512)
+
+# Material parameters
 eps_Si = 11.7  # Si dielectric function in the mid-infrared
 eps_environment = 1  # Vacuum/air dielectric function
 refl_coeff = pysnom.reflection.refl_coeff(eps_environment, eps_Si)
@@ -15,10 +17,10 @@ alpha_eff = pysnom.fdm.eff_pol_0_bulk(z=z, beta=refl_coeff)
 # Plot output
 fig, ax = plt.subplots()
 z_nm = z * 1e9  # For neater plotting
-ax.plot(z_nm, np.abs(alpha_eff))
+ax.plot(z_nm, alpha_eff.real)
 ax.set(
     xlabel=r"$z$ / nm",
-    ylabel=r"$|\alpha_{eff}|$",
+    ylabel=r"$\Re(\alpha_{eff})$",
     xlim=(z_nm.min(), z_nm.max()),
 )
 fig.tight_layout()
--- docs/guide/plots/basics_modulated.py
+++ docs/guide/plots/basics_modulated.py
@@ -3,17 +3,20 @@ import numpy as np
 
 import pysnom
 
-# Set some experimental parameters for an AFM approach curve
-tapping_freq = 250e3
-tapping_amplitude = 25e-9
-z_0 = 10e-9
-periods = 3
-t = np.linspace(-periods * np.pi / tapping_freq, periods * np.pi / tapping_freq, 512)
-z = z_0 + tapping_amplitude * (1 + np.cos(tapping_freq * t))
+# Set some experimental parameters for an oscillating AFM probe
+tapping_freq = 2 * np.pi * 250e3  # 250 kHz tapping frequency
+tapping_amplitude = 25e-9  # 25 nm oscillation amplitude
+z_0 = 10e-9  # 10 nm from sample at bottom of oscillation
+periods = 3  # Number of oscillations to show
 
+# Material parameters
 eps_Si = 11.7  # Si dielectric function in the mid-infrared
 eps_environment = 1  # Vacuum/air dielectric function
 refl_coeff = pysnom.reflection.refl_coeff(eps_environment, eps_Si)
+
+# Find z as a function of t
+t = np.linspace(-periods * np.pi / tapping_freq, periods * np.pi / tapping_freq, 512)
+z = z_0 + tapping_amplitude * (1 + np.cos(tapping_freq * t))
 
 # Calculate the effective polarisability
 alpha_eff = pysnom.fdm.eff_pol_0_bulk(z=z, beta=refl_coeff)
@@ -23,11 +26,11 @@ fig, axes = plt.subplots(nrows=2, sharex=True)
 t_us = t * 1e6  # For neater plotting
 z_nm = z * 1e9  # For neater plotting
 axes[0].plot(t_us, z_nm)
+axes[1].plot(t_us, alpha_eff.real)
 axes[0].set(ylabel=r"$z$ / nm", ylim=(0, None))
-axes[1].plot(t_us, np.abs(alpha_eff))
 axes[1].set(
     xlabel=r"$t$ / $\mathrm{\mu}$s",
-    ylabel=r"$|\alpha_{eff}|$",
+    ylabel=r"$\Re(\alpha_{eff})$",
     xlim=(t_us.min(), t_us.max()),
 )
 fig.tight_layout()
